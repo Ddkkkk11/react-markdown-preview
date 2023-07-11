0d2700efcc164b0afe21595ae8c256d96bb46faf
顶级压力小子 <45672942@qq.com>
2023-07-12 00:28:53 +0800

    style(preview): 修改li ol 样式

--- src/commonpents/preview/index.tsx
+++ src/commonpents/preview/index.tsx
@@ -2,10 +2,13 @@ import ReactMarkdown from 'react-markdown'
 import './style.less'
 export default function Preview(props: any) {
     const { text } = props;
+    console.log(text, 'text');
 
   return (
     <section className='preview-area'>
-      <ReactMarkdown children={text}/>
+        <div className='markdown-preview'>
+            <ReactMarkdown children={text}/>
+        </div>
     </section>
   )
 }
